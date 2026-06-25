b6c571918b451a0d7c752947250f0140a937b7dc
Arvind <jsrarvind12@gmail.com>
2026-06-25 20:39:56 +0530

    done with header and cookie handling in middleware

--- route-handler-demo/middleware.ts
+++ route-handler-demo/middleware.ts
@@ -14,10 +14,24 @@ import type { NextRequest } from "next/server";
 
 // second conditional method for handling redirection
 
-export function middleware(request:NextRequest){
-  if(request.nextUrl.pathname==="/profile"){
-    // return NextResponse.redirect(new URL("/hello",request.nextUrl));
-        return NextResponse.rewrite(new URL("/hello",request.nextUrl)); // if you want that url shouldn't be change but content should be shown of that target url use rewrite intead of redirect
+// export function middleware(request:NextRequest){
+//   if(request.nextUrl.pathname==="/profile"){
+//     // return NextResponse.redirect(new URL("/hello",request.nextUrl));
+//         return NextResponse.rewrite(new URL("/hello",request.nextUrl)); // if you want that url shouldn't be change but content should be shown of that target url use rewrite intead of redirect
 
+//   }
+// }
+
+
+// Handling cookie via middleware
+
+export function middleware(request:NextRequest){
+  const response = NextResponse.next();
+  const themePreference = response.cookies.get("theme");
+  if(!themePreference){
+    response.cookies.set("theme","dark");
   }
+
+  response.headers.set("custom-header","custom-value");
+  return response;
 }
