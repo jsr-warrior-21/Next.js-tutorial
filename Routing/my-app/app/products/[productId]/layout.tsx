280b195c870d442c69968ec50129890183b26d41
Arvind <jsrarvind12@gmail.com>
2026-06-27 15:14:34 +0530

    adding more content in nested layout in productId

--- Routing/my-app/app/products/[productId]/layout.tsx
+++ Routing/my-app/app/products/[productId]/layout.tsx
@@ -3,6 +3,9 @@ import React from "react";
 export default function productLayout({children}:{children:React.ReactNode}){
     return (
         <>
+        <header className="text-green-300 h-auto w-auto">
+                I am nested Layout.
+        </header>
         {children}
         <h1>Feature Of Product (from nested Layout)</h1>
         </>
