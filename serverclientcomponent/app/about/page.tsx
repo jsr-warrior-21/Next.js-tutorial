fbe372261c162ea4f3945dfc7d480a5451b2b1d5
Arvind <jsrarvind12@gmail.com>
2026-06-29 18:58:55 +0530

    dynamic rendering

--- serverclientcomponent/app/about/page.tsx
+++ serverclientcomponent/app/about/page.tsx
@@ -1,6 +1,13 @@
-export default function about() {
-    console.log("About server component.") // in console you will see that this is a sever componet with server tag.
-  return <>
-        <h1>New About page for server Client component.</h1>
-  </>;
+import { cookies } from "next/headers";
+
+export default async function about() {
+  const cookieTheme = await cookies();
+  const getTheme = cookieTheme.get("theme");
+  console.log(getTheme);
+  console.log("About server component."); // in console you will see that this is a sever componet with server tag.
+  return (
+    <>
+      <h1>New About page for server Client component.</h1>
+    </>
+  );
 }
