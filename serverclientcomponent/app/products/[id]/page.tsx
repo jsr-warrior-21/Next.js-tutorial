cc683488540a8aa61491ca988d788a8af49527b7
Arvind <jsrarvind12@gmail.com>
2026-06-29 19:42:59 +0530

    using generateStaticParams() for static rendering of some files which i will get prerendered

---
+++ serverclientcomponent/app/products/[id]/page.tsx
@@ -0,0 +1,12 @@
+export async function generateStaticParams(){
+    return [{id:"1"},{id:"2"},{id:"3"}];
+}
+
+export default async function productDetails({params}:{params:Promise<{id:string}>}){
+    const id =  (await params).id;
+    return(
+        <>
+            <h1>Details of Product with product id : {id} created on {new Date().toLocaleTimeString()}</h1>       
+        </>
+    )
+}
