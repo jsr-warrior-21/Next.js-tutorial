b36f93169e73fa176ec3d28bbc7e27fd501a4578
Arvind <jsrarvind12@gmail.com>
2026-07-02 17:58:27 +0530

    done with client-only safegaurd

--- serverclientcomponent/app/client-route/page.tsx
+++ serverclientcomponent/app/client-route/page.tsx
@@ -1,14 +1,16 @@
 "use client";
 import serverFunction from "@/utils/server-utils";
 import { useTheme } from "@/components/theme-provider";
+import { clientSideFunction } from "@/utils/client-utils";
 
 export default function clientRoute() {
-    const result = serverFunction();
-    const theme = useTheme();
+  const result = serverFunction();
+  const theme = useTheme();
+  const clientResult = clientSideFunction();
   return (
     <>
-
-      <h1 style={{color:theme.colors.primary}}>Client-Route : {result}</h1>
+      <h1 style={{ color: theme.colors.primary }}>Client-Route : {result}</h1>
+      <p>{clientResult}</p>
     </>
   );
 }
