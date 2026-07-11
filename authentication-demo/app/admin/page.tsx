b2e17494e185e51212af7c58a18eb7809000ef2a
Arvind <jsrarvind12@gmail.com>
2026-07-11 13:19:23 +0530

    done with route based access controll (RBAC) system

---
+++ authentication-demo/app/admin/page.tsx
@@ -0,0 +1,7 @@
+export default function AdminPage() {
+  return (
+    <>
+      <h1>Admin Only Page</h1>
+    </>
+  );
+}
--- authentication-demo/middleware.ts
+++ authentication-demo/middleware.ts
@@ -1,10 +1,18 @@
 import { clerkMiddleware} from '@clerk/nextjs/server'
 import { createRouteMatcher } from '@clerk/nextjs/server';
+import { NextResponse } from 'next/server';
 
 const isProtectedRoute = createRouteMatcher(["/user-profile"]);
+const isAdminRoute = createRouteMatcher(["/admin(.*)"]);
 
 export default clerkMiddleware(async(auth,req)=>{
+
+    if(isAdminRoute(req) && (await auth()).sessionClaims?.metadata.role!=="admin"){
+      const url = new URL("/",req.url);
+      return NextResponse.redirect(url);
+    }
     if(!isProtectedRoute(req)) await auth.protect();
+
 })
 
 export const config = {
